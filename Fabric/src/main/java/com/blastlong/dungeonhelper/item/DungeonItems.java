bf56c9dbf7746851c46685aba486929216e1fd6a
BlastLong <vertblast@naver.com>
2023-02-15 09:06:40 +0900

    fix: adjust item json to 1.19.3

--- Fabric/src/main/java/com/blastlong/dungeonhelper/item/DungeonItems.java
+++ Fabric/src/main/java/com/blastlong/dungeonhelper/item/DungeonItems.java
@@ -20,20 +20,11 @@ public class DungeonItems {
     public static final Item MYTHIC_BOOK = new Item(new Item.Properties());
 
     public static void register() {
-        /*
         Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(DungeonHelper.MODID, "common_book"), COMMON_BOOK);
         Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(DungeonHelper.MODID, "uncommon_book"), UNCOMMON_BOOK);
         Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(DungeonHelper.MODID, "rare_book"), RARE_BOOK);
         Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(DungeonHelper.MODID, "epic_book"), EPIC_BOOK);
         Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(DungeonHelper.MODID, "legendary_book"), LEGENDARY_BOOK);
         Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(DungeonHelper.MODID, "mythic_book"), MYTHIC_BOOK);
-        */
-
-        ItemGroupEvents.modifyEntriesEvent(MinecraftItemGroups.TOOLS_ID).register(entries -> entries.addAfter(Items.MUSIC_DISC_PIGSTEP, COMMON_BOOK));
-        ItemGroupEvents.modifyEntriesEvent(MinecraftItemGroups.TOOLS_ID).register(entries -> entries.addAfter(COMMON_BOOK, UNCOMMON_BOOK));
-        ItemGroupEvents.modifyEntriesEvent(MinecraftItemGroups.TOOLS_ID).register(entries -> entries.addAfter(UNCOMMON_BOOK, RARE_BOOK));
-        ItemGroupEvents.modifyEntriesEvent(MinecraftItemGroups.TOOLS_ID).register(entries -> entries.addAfter(RARE_BOOK, EPIC_BOOK));
-        ItemGroupEvents.modifyEntriesEvent(MinecraftItemGroups.TOOLS_ID).register(entries -> entries.addAfter(EPIC_BOOK, LEGENDARY_BOOK));
-        ItemGroupEvents.modifyEntriesEvent(MinecraftItemGroups.TOOLS_ID).register(entries -> entries.addAfter(LEGENDARY_BOOK, MYTHIC_BOOK));
     }
 }
